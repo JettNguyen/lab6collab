ac923cc31a4d5c204ad2877fff0687deab489b44
Diego Valdivia <dvaldivia@ufl.edu>
2023-03-22 14:58:03 -0400

    added decode function and fixed a small issue in the encode function when the sum goes over 10. :)

--- lab6collab.py
+++ lab6collab.py
@@ -18,8 +18,34 @@ def main():
 def encode(password):
     encoded = ''
     for char in password:
-        encoded += (str(int(char) + 3))  # adds 3 to each number in the password
+
+        if int(char) + 3 > 10:                  # Diego: added to check if the sum is over 10
+
+            encoded += str((int(char)+3) % 10)  # Diego: added to take only the last digit if the sum is over 10
+                                                # example if 9 input 9 + 3 = 12 take only 2
+        else:
+
+            encoded += (str(int(char) + 3))  # adds 3 to each number in the password
+
     return encoded
+
+
+def decode(password_encoded):
+
+    password_decoded_list = []
+
+    for i in range(0, len(password_encoded)):
+        password_decoded_list.append(int(password_encoded[i]) - 3)
+
+        if password_decoded_list[i] < 0:
+            password_decoded_list[i] = password_decoded_list[i] + 10
+
+    password_decoded = ''
+
+    for j in range(0, len(password_decoded_list)):
+        password_decoded += str(password_decoded_list[j])
+
+    return password_decoded
 
 
 if __name__ == "__main__":
